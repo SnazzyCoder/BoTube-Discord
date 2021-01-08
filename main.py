ec3354ee58f4f3957a08f7cc0b35266e20050055
snazzycoder <snazzycoder@gmail.com>
2021-01-08 19:42:51 +0530

    Added Cjanges

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import config
 import messages.general as msg
 from discord.ext import commands
 import discord
+from discord.ext.commands import CommandNotFound
 
-bot = commands.Bot("b!")
+prefix = 'b!'
+bot = commands.Bot(prefix)
 
 # Ready Message Printer
 @bot.event
@@ -34,6 +36,23 @@ async def who(ctx, *args):
         print(f"{ctx.author.mention} is the greatest Discorder that's been alive.")
         await ctx.send(f"{ctx.author.mention} is the greatest Discorder that's been alive.")
     else:
-        await(f"{', '.join([i for i in str(args).raw_mentions.mention()])}, stop being spherical dumbass")
+        await ctx.send(f"{', '.join([str(i) for i in ctx.message.raw_mentions.mention()])}, stop being spherical dumbass")
+
+@bot.command()
+async def pre(ctx, new_prefix, *arg):
+    if arg:
+        await ctx.send(f'This command only requries 1 argument in the manner `{pre}prefix <new prefix>`')
+        return
+    
+    prefix = new_prefix
+    bot = commands.Bot(prefix)
+
+
+# Command not found error!
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        await ctx.send(f'{ctx.message} is not recognized as a valid command.\n\n Send `{prefix}help` to know what I can do')
+
 
 bot.run(config.TOKEN)
